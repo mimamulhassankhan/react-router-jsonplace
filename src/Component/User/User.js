7fdf8ebf2b4435018f02ac1d33fc80ab304ffb7a
mimamulhassankhan <pappuhassan440@gmail.com>
2020-09-13 11:51:26 +0600

    two ways dynamic route added

--- src/Component/User/User.js
+++ src/Component/User/User.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const User = (props) => {
+    let history = useHistory();
     const {id, name, email} = props.friend;
-    console.log(props.friend);
+    const clickHandler = (friendId) => {
+        let url = `/friend/${friendId}`;
+        history.push(url);
+    }
     
     return (
         <div className="border text-center border-danger m-4 p-3 rounded">
             <h2>Name : {name}</h2>
             <h3>Email : {email}</h3>
             <Link to={`/friend/${id}`} className="btn btn-success">Know About {id}</Link>
+            <button onClick={() => clickHandler(id)} className="btn btn-danger ml-1">Click Me</button>
         </div>
     );
 };
